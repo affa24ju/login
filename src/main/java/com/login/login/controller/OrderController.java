a968a62de772c47783997c29f3c8c5f2189040c1
Fatema Afroza <fafroza2017@gmail.com>
2025-01-16 21:42:42 +0100

    uppdaterad order controller

--- src/main/java/com/login/login/controller/OrderController.java
+++ src/main/java/com/login/login/controller/OrderController.java
@@ -1,13 +1,15 @@
 package com.login.login.controller;
 
-import org.springframework.security.access.annotation.Secured;
+//import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderController {
     
-    @Secured("ROLE_USER")
+    //bara inloggade user med roll "user" kan kommat åt till orders
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/orders")
     public String orderPage(){
         return "orders";
