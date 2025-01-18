9c91078f64abe0c5453c0fc7f0558ee71089a52e
Fatema Afroza <fafroza2017@gmail.com>
2025-01-18 23:07:52 +0100

    småfix

--- src/main/java/com/login/login/controller/OrderController.java
+++ src/main/java/com/login/login/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.login.login.controller;
 
-//import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
